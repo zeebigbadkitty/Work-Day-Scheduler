e40f72051794599a44d41aa19940461bd1de4b89
Socially Awkward Streams <candice.radam@gmail.com>
2022-10-19 11:28:33 -0500

    Pseudo coding and adding some JS

--- script.js
+++ script.js
@@ -6,9 +6,18 @@ document.getElementById("currentDay").textContent = currentDay;
 
 // WHEN I view the timeblocks for that day
 // THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+if (currentDay < moment('HH')){
+    // Past, Present, Future
+
+}
 // WHEN I click into a timeblock
 // THEN I can enter an event
 // WHEN I click the save button for that timeblock
+var event=localStorage.getItem("event")
+
 // THEN the text for that event is saved in local storage
+localStorage.setitem "JSON STRINGIFY"
 // WHEN I refresh the page
 // THEN the saved events persist
+
+//if === add class "present"
