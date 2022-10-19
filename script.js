68de55f060f8c207fcd1d1031ad24cff7497e7a7
Socially Awkward Streams <candice.radam@gmail.com>
2022-10-19 11:53:29 -0500

    If statement to change classes.

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 
 // The current day is displayed at the top of the calendar
-var currentDay = moment().format('MMMM Do YYYY, h:mm:ss a');
-document.getElementById("currentDay").textContent = currentDay;
+var currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
+document.getElementById("currentDay").textContent = currentTime;
 
 
 
 // Each timeblock is color coded to indicate whether it is in the past, present, or future (Check currentDay and add class)
-if (currentDay < moment('HH')){
-
+if (currentTime < moment('HH')){
+    $().addClass("past");
+}
+else if (currentTime > moment('HH')){
+    $().addClass("future");
+}
+else if (currentTime === moment('HH')){
+    $().addClass("present");
 }
 
 // I click the save button for that timeblock the text for that event is saved in local storage
